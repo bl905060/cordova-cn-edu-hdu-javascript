3a50f487a1dbcbb710676d4c38d66231833dd9e5
Casper <bilei905060@icloud.com>
2015-09-24 20:52:46 +0800

    add multi-table insert

--- www/operateDB.js
+++ www/operateDB.js
@@ -9,17 +9,32 @@
     example: operateDB.queryData('goodslist', '*', 'id = ?', [id], readData);
 
     insert:function saveData
-    @param count, tablename, insertFields, insertParams, callback
-        count:需要插入的记录数目
-        tablename:表名
-        insertFields:需要插入的字段
-        insertParams:需要插入的数据
+    @param option, tablename, insertFields, insertParams, callback
+        option:[需要插入的表数，第一张表需要插入的记录数，第二张表需要插入的记录数...以此类推]
+        tablename:[第一张表名，第二张表名...以此类推]
+        insertFields:需要插入的字段，二维数据结构存储每张表的字段
+        insertParams:需要插入的数据，二维数组结构存储每张表的数据
         callback:插入成功后调用的函数
     example:
-        var insertField = ['user_id', 'goods_id', 'goods_name', 'goods_barcode', 'goods_brand', 'goods_category', 'goods_descr', 'goods_unit', 'goods_manufacturer', 'goods_stockprice', 'goods_saleprice', 'quantity', 'goods_status', 'goods_remark', 'photo_id', 'photo_url', 'timestamp', 'delflag', 'goods_ntu'];
-        var insertParam = [user_id, goods_id, goods_name, goods_barcode, goods_brand, goods_category, goods_descr, goods_unit, goods_manufacturer, goods_stockprice, goods_saleprice, quantity, goods_status, goods_remark, photo_id, photo_url, timestamp, delflag, goods_ntu];
+        var insertField = new Array();//生成插入字段的一维数组
+        var insertParam = new Array();//生成插入数据的一位数组
+        
+        //以二维数组方式生成第一张数据表需要插入的字段
+        insertField[1] = new Array('org_id', 'user_id', 'goods_id', 'goods_name', 'goods_barcode', 'goods_brandid', 'goods_categoryid', 'goods_unitid', 'goods_manufacturerid', 'goods_descr', 'goods_stockprice', 'goods_price', 'goods_photoidprefix', 'publishflag');
+        //以二维数组方式生成第一张数据表需要插入的数据
+        insertParam[1] = new Array(org_id, user_id, goods_id, goods_name, goods_barcode, goods_brandid, goods_categoryid, goods_unitid, goods_manufacturerid, goods_descr, goods_stockprice, goods_price, goods_photoidprefix, publishflag);
+ 
+        //以二维数组方式生成第二张数据表需要插入的字段
+        insertField[2] = new Array('user_id', 'photo_id', 'localurl');
+        //以二维数组方式生成第二张数据表需要插入的字段
+        insertParam[2] = new Array(user_id, goods_photoidprefix[1], localURL[1], user_id, goods_photoidprefix[2], localURL[2]);
+        
+        //将需要插入的数据表放入一维数组中
+        var tableName = new Array('myshop_simplegoods', 'myshop_photo');
+        //第一位为需要插入的数据表总数，第二位开始为每张表需要插入的记录条目，本例中第一张表需要插入1条记录，第二张表需要插如2条记录
+        var DBoption = new Array('2', '1', '2');
          
-        operateDB.saveData(count, 'goodslist', insertField, insertParam, saveSuccess);
+        operateDB.saveData(option, tablename, insertField, insertParam, saveSuccess);
 
     update:function updateData
     @param tableName, setFileds, setParams, whereStr, whereParams, callback
@@ -84,46 +99,74 @@ var operateDB = new Object({
         }
     },
 
-    saveData : function (count, tableName, insertFields, insertParams, callback) {
+    saveData : function (option, tableName, insertFields, insertParams, callback) {
         console.log("saveDB is begin!");
         
         var mydb = operateDB.openDB();
         
-        var sql = 'INSERT INTO ' + tableName + ' (';
-        var params = ' (';
+        var tableCount = option[0];
+        var sql = new Array();
+        var params = new Array();
+        var successCount = 0;
         
-        for (i in insertFields) {
-            sql += insertFields[i] + ', ';
-            params += '?, ';
-        }
-        sql = sql.substr(0, sql.length - 2);
-        params = params.substr(0, params.length - 2);
-        sql += ')';
-        params += ')';
-        
-        var temp = params;
+        alert(tableCount);
                            
-        while (count > 1) {
-            temp = temp + ', ' + params;
-            count--;
-        }
+        for (c=1; c<=tableCount; c++) {
+             sql[c] = 'INSERT INTO ' + tableName[c-1] + ' (';
+             params[c] = ' (';
                            
-        params = temp;
+            for (i in insertFields[c]) {
+                sql[c] += insertFields[c][i] + ', ';
+                params[c] += '?, ';
+            }
+            
+            sql[c] = sql[c].substr(0, sql[c].length - 2);
+            params[c] = params[c].substr(0, params[c].length - 2);
+            sql[c] += ')';
+            params[c] += ')';
+             
+            //alert(sql[c]);
+            //alert(params[c]);
+
+            var temp = params[c];
+            var recordcount = option[c];
+       
+            //alert(temp);
+            //alert(recordcount);
+                           
+            while (recordcount > 1) {
+                temp = temp + ', ' + params[c];
+                recordcount--;
+            }
+       
+            params[c] = temp;
+       
+            console.log(sql[c] + ' VALUES ' + params[c]);
+            console.log(insertParams[c]);
+            //alert(sql[c] + ' VALUES ' + params[c]);
+            //alert(insertParams[c]);
+        }
         
-        console.log(sql + ' VALUES ' + params);
-        console.log(insertParams);
-        
+                
         mydb.transaction(insertData, errorData);
+        
+        callback();
         
         function insertData(tx) {
             console.log("sql is ready to run!");
-            tx.executeSql(sql + ' VALUES ' + params, insertParams, insertSuccess, errorData);
+            for (c=1; c<=tableCount; c++) {
+                tx.executeSql(sql[c] + ' VALUES ' + params[c], insertParams[c], insertSuccess, errorData);
+            }
             console.log("sql is executed!");
         }
         
         function insertSuccess() {
             console.log("Data write Success!");
-            callback();
+            successCount++;
+            alert(successCount);
+            if (successCount == tableCount) {
+                callback();
+            }
         }
         
         function errorData(err) {
@@ -157,7 +200,6 @@ var operateDB = new Object({
         
         function updateSuccess() {
             console.log("Data renew is done!");
-            callback();
         }
         
         function errorData(err) {
