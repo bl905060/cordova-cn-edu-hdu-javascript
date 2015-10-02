d8c0bc64c752e3ed80cc6cec2b3b643179d1209a
Casper <bilei905060@icloud.com>
2015-10-02 09:42:04 +0800

    fix bug

--- www/getMedia.js
+++ www/getMedia.js
@@ -115,7 +115,12 @@ var getMedia = new Object ({
                     ic = document.getElementById(showImgDivId);
                     //Then write an image tag out to the div using the
                     //URL we received from the camera application.
-                    ic.innerHTML = ic.innerHTML + '<img id="' + showImgId + '" src="' + fileEntry.toURL() + '" width=100% title="' + photoidprefix + '" />';
+                    if (option[3] > 1) {
+                        ic.innerHTML = ic.innerHTML + '<img id="' + showImgId + '" src="' + fileEntry.toURL() + '" width=100% title="' + photoidprefix + '" />';
+                    }
+                    else {
+                        ic.innerHTML = '<img id="' + showImgId + '" src="' + fileEntry.toURL() + '" width=100% title="' + photoidprefix + '" />';
+                    }
                     console.log(document.getElementById(showImgId).src);
                     console.log(document.getElementById(showImgId).title);
                     console.log(showImgId);
